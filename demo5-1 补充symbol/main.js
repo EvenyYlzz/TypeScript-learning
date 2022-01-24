ad28f3dc0da66144104f79abaccf1b2208f6f696
EvenyYlzz <172463074@qq.com>
2022-01-24 23:44:34 +0800

    feat: demo5-1 Symbol

---
+++ demo5-1 补充symbol/main.js
@@ -0,0 +1,17 @@
+var s1 = Symbol();
+var s2 = Symbol();
+console.log(s1); // Symbol()
+console.log(typeof s1); // symbol
+console.log(s1 === s2); // false
+// ----------------------------------------------------
+var s3 = Symbol();
+var s4 = Symbol();
+console.log(s3); // Symbol()
+console.log(typeof s3); // symbol
+console.log(s3 === s4); // false
+// ----------------------------------------------------
+var s5 = Symbol["for"]('foo');
+var s6 = Symbol["for"]('foo');
+// Symbol.fo会去全局查找是否已经存在对应标识符对应的symbol值
+// 如果存在，直接返回，如果不存在，则新建一个symbol值
+console.log(s5 === s6); // => true
---
+++ demo5-1 补充symbol/main.ts
@@ -0,0 +1,24 @@
+const s1 = Symbol()
+const s2 = Symbol()
+
+console.log(s1) // Symbol()
+console.log(typeof s1) // symbol
+console.log(s1 === s2) // false
+
+// ----------------------------------------------------
+
+const s3: symbol = Symbol()
+const s4: symbol = Symbol()
+
+console.log(s3) // Symbol()
+console.log(typeof s3) // symbol
+console.log(s3 === s4) // false
+
+// ----------------------------------------------------
+
+const s5: symbol = Symbol.for('foo')
+const s6: symbol = Symbol.for('foo')
+
+// Symbol.fo会去全局查找是否已经存在对应标识符对应的symbol值
+// 如果存在，直接返回，如果不存在，则新建一个symbol值
+console.log(s5 === s6) // => true
